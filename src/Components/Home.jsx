6a29908a7ec0c3c51b1e631f61d7ba06bbc708b3
Arittro7 <arittrowpdev@gmail.com>
2025-01-30 12:39:02 +0600

    local operation done

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import AddAddress from "./AddAddress";
+import SavedAddress from "./SavedAddress";
+
+const Home = () => {
+  return (
+    <div>
+      <AddAddress></AddAddress>
+      <SavedAddress></SavedAddress>
+    </div>
+  );
+};
+
+export default Home;
